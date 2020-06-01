1d81c74c5f6784a9fe10317120ef7e63b49a839d
sayalipawade <sayalipawade05@gmail.com>
2020-06-01 21:34:27 +0530

    Added:Gambler playing game until win or lost

--- Gambling.py
+++ Gambling.py
@@ -1,30 +1,55 @@
 import random
 
-# Taking input from user
-stake_amount = int(input("Enter stake amount : "))
-goal_amount = int(input("Enter goal amount : "))
-bet_amount = int(input("Enter bet amount : "))
-number_of_times = int(input("Enter number of times you want to play : "))
-
 # Variables
 win_count = 0
 loss_count = 0
+stake_amount = 0
+goal_amount = 0
+bet_amount = 0
 
-# loop for iterating no of times
-for index in range(1, number_of_times+1):
-    random_win = random.randint(0, 1)
-    if random_win == 0:
-        stake_amount -= bet_amount
-        loss_count += 1
-    else:
-        stake_amount += bet_amount
-        win_count += 1
+#Constants
+NUMBER_OF_TIMES=20
 
-# Calculating win and loss percentage
-win_percentage = win_count*100/number_of_times
-print("win percentage is:", win_percentage)
-loss_percentage = loss_count*100/number_of_times
-print("loss percentage is:", loss_percentage)
+class GamblingSimulator:
+
+    # Taking input from user
+    def take_input(self):
+        stake_amount = int(input("Enter stake amount : "))
+        goal_amount = int(input("Enter goal amount : "))
+        bet_amount = int(input("Enter bet amount : "))
+        
+    # user playing game
+    def play_game(self):
+        global NUMBER_OF_TIMES
+        global stake_amount
+        global goal_amount
+        global win_count
+        global loss_count
+        for index in range(1, NUMBER_OF_TIMES+1):
+            random_win = random.randint(0, 1)
+            if random_win == 0:
+                stake_amount -= bet_amount
+                loss_count += 1
+            else:
+                stake_amount += bet_amount
+                win_count += 1
+
+    #Percentage for win and loss
+    def calculate_percentage(self):
+        win_percentage = win_count*100/NUMBER_OF_TIMES
+        print("win percentage is:", win_percentage)
+        loss_percentage = loss_count*100/NUMBER_OF_TIMES
+        print("loss percentage is:", loss_percentage)
+
+#Creating object of class
+gambler=GamblingSimulator()
+
+#Function Calling
+gambler.take_input()
+gambler.play_game()
+gambler.calculate_percentage()
+
+
 
 
 
