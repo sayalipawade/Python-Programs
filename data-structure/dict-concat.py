d3d0a6e31a2ef86c5470d588706117a240d53329
sayalipawade <sayalipawade05@gmail.com>
2020-06-01 21:50:25 +0530

    Added:Concatenation of two Dictionaries

---
+++ data-structure/dict-concat.py
@@ -0,0 +1,9 @@
+#Dictionaries
+dict1={1:"Sayali",2:"pooja"}
+dict2={3:"kiran",4:"shamali"}
+dict3={5:"kalyani",6:"Dayani"}
+
+#Concatnation of dictionaries
+dict1.update(dict2)
+dict1.update(dict3)
+print(dict1)
