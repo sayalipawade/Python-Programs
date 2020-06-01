a23318f127b27fe8838dcf54b834cecc67837417
sayalipawade <sayalipawade05@gmail.com>
2020-06-01 22:23:50 +0530

    Added:Joining of two tuples

---
+++ data-structure/join_tuples.py
@@ -0,0 +1,9 @@
+#Joining tuple
+def join_tuple(tuple1,tuple2):
+    new_tuple=tuple1+tuple2
+    print(new_tuple)
+
+#tuples
+tuple1=("sayali","prapti","pratiksha")
+tuple2=(1,2,3,4,5)
+join_tuple(tuple1,tuple2)
