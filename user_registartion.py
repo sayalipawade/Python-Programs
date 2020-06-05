6ffcb0aca54c2e5f35ac43f02cfde93bd39b87cc
sayalipawade <sayalipawade05@gmail.com>
2020-06-05 10:51:00 +0530

    Added:Pattern Validation using regex pattern

---
+++ user_registartion.py
@@ -0,0 +1,39 @@
+import re
+
+class UserRegistration:
+
+    #constants
+    valid_name_pattern='^[A-Z]([a-zA-Z]{2,})$'
+    valid_mobile_pattern='^[0-9]{2}[0-9]{10}'
+
+    #Checking First name is valid or not
+    def first_name_checking(self):
+        first_name=(input("Enter first name:"))
+        match=re.match(self.valid_name_pattern,first_name)
+        if match:
+            print("First name is valid")
+        else:
+            print("First name is Invalid")
+
+    #Checking Last name is valid or not
+    def last_name_checking(self):
+        last_name=(input("Enter last name:"))
+        match=re.match(self.valid_name_pattern,last_name)
+        if match:
+            print("Last name is valid")
+        else:
+            print("Last name is Invalid")
+
+    #Checking Mobile no is valid or not
+    def mobile_number_checking(self):
+        mobile_number=(input("Enter mobile no:"))
+        match=re.match(self.valid_mobile_pattern,mobile_number)
+        if match:
+            print("Mobile no is valid")
+        else:
+            print("Mobile no is Invalid")
+
+user=UserRegistration()
+user.first_name_checking()
+user.last_name_checking()
+user.mobile_number_checking()
