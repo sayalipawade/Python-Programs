8a1c2e5bbda7cb89bee6ec165d57ba4669497843
sayalipawade <sayalipawade05@gmail.com>
2020-06-09 13:28:37 +0530

    Refactored:Changed in variable names

--- swap_nibbles.py
+++ swap_nibbles.py
@@ -3,19 +3,14 @@ print("Welcome to swap nibbles")
 class Swap_nibbles:
 
     #Variable
-    x=0
+    number=0
 
     #method for swapping nibbles
-    def swapping_nibbles(self,x): 
-        return ( (x & 0x0F)<<4 | (x & 0xF0)>>4 )
+    def swapping_nibbles(self,number): 
+        return ( (number & 0x0F)<<4 | (number & 0xF0)>>4 )
 
 #Object creation
 swap_nibble=Swap_nibbles()
-x=int(input("Enter number:"))
-result=swap_nibble.swapping_nibbles(x)
-print(result)
-
-
-
-
-  
+number=int(input("Enter number:"))
+result=swap_nibble.swapping_nibbles(number)
+print("After swapping nibbles number is:",result)  
